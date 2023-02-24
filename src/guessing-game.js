62415d1ab3290ad005b3bafeaee6d9afa9828596
samael-god <nikita.95_@mail.ru>
2023-02-24 12:05:29 +0300

    feat: finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.try = Math.ceil(((this.max + this.min) / 2));
+        return this.try;
     }
 
     lower() {
-
+        this.max = this.try;
     }
 
     greater() {
-
+        this.min = this.try;
     }
 }
 
